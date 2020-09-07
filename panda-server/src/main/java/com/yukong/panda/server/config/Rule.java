a128e7b6f286562daa0196021cceea126b675ad0
Dachang.Yu <dachyu@163.com>
2020-09-07 08:03:16 +0800

    7/9

---
+++ panda-server/src/main/java/com/yukong/panda/server/config/Rule.java
@@ -0,0 +1,17 @@
+package com.yukong.panda.server.config;
+
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+
+@Configuration
+public class Rule {
+
+    @Bean
+    @Scope("prototype")//这个注解不能少,否则快速调用几次就会出现com.netflix.client.ClientException: Load balancer does not have available server for client 异常
+    public IRule tagRule() {
+        return new CustomIsolationRule();
+    }
+}
